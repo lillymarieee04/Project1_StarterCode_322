9f37702e92320035da9f31e7ba012e506ad308cb
lillymarieee04 <lillywat2023@gmail.com>
2026-03-09 22:57:05 -0400

    Step 5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        double[] fullSound = StdAudio.read("assets/cardinal_trim.wav");
+        System.out.println("Total samples: " + fullSound.length);
+        StdAudio.play(fullSound);
+        StdAudio.drain();
+
     }
 }
