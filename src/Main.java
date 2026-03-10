4412b446e4582a9e2b598ff3092ea2231cc7a668
lillymarieee04 <lillywat2023@gmail.com>
2026-03-09 23:05:04 -0400

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-
-        double[] fullSound = StdAudio.read("assets/cardinal_trim.wav");
-        System.out.println("Total samples: " + fullSound.length);
-        StdAudio.play(fullSound);
-        StdAudio.drain();
-
-        int start = 0;
-        int end = fullSound.length / 3;
-
-        double[] oneBird = new double[end - start];
-        for (int i = 0; i < oneBird.length; i++) {
-            oneBird[i] = fullSound[start + i];
-        }
-
-        StdAudio.play(oneBird);
-        StdAudio.drain();
-
+        System.out.println("Hello world!");
     }
 }
